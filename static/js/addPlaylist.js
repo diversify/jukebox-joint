e720980a0c8ced73e8ac76e2e445aef41f94983e
wiwofone <apeirion@gmail.com>
2014-11-22 23:45:24 +0100

    Show previous playlists

--- static/js/addPlaylist.js
+++ static/js/addPlaylist.js
@@ -1,7 +1,16 @@
 angular.module('jukeboxApp')
-  .controller('AddPlaylistCtrl', function ($rootScope, $scope, Playlist, $location) {
+  .controller('AddPlaylistCtrl', function ($rootScope, $scope, Playlist, $location, $http) {
     
     $scope.playlist = {};
+    
+    $http.get('/get-user-playlists/' + 'gaeamearth1').
+        success(function(data, status, headers, config) {
+          $scope.playlists = data;
+          console.log(data);
+        }).
+        error(function(data, status, headers, config) {
+          console.log('Dun goofed');
+        });
     
     /* Called by view, saves a playlist */
     $scope.addPlaylist = function()
@@ -14,5 +23,5 @@ angular.module('jukeboxApp')
       });
         
     };
-    
+        
   });
