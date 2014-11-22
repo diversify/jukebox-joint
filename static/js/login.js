4987b0703f52a2c2e6e4ae0b68ec528d7fb8d9ee
wiwofone <apeirion@gmail.com>
2014-11-22 20:33:55 +0100

    Redirect to add playlist after login

--- static/js/login.js
+++ static/js/login.js
@@ -1,11 +1,12 @@
 angular.module('jukeboxApp')
-  .controller('LoginCtrl', function ($rootScope, $scope, Spotify) {
+  .controller('LoginCtrl', function ($rootScope, $scope, Spotify, $location) {
 
     $scope.loginUser = function()
     {
         Spotify.login().then(function(data) {
           Spotify.getCurrentUser().then(function(data) {
             $rootScope.user = data;
+            $location.path( "/addPlaylist" );
           });
         });
     };
