d8216ad3fd93b8dab5fd8fcf50555d135df5177f
wiwofone <apeirion@gmail.com>
2014-11-22 22:34:42 +0100

    Remove old API methods, we're doing this backend

--- static/js/addPlaylist.js
+++ static/js/addPlaylist.js
@@ -1,23 +1,18 @@
 angular.module('jukeboxApp')
-  .controller('AddPlaylistCtrl', function ($rootScope, $scope, Spotify, Playlist, $location) {
+  .controller('AddPlaylistCtrl', function ($rootScope, $scope, Playlist, $location) {
     
     $scope.playlist = {};
     
     /* Called by view, saves a playlist */
     $scope.addPlaylist = function()
     {
-      /* Save it in Spotify */
-      Spotify.createPlaylist($rootScope.user.id, {name: $scope.playlist.name}).then(function(data) {
-        $scope.playlist.playlistId = data.id;
-        $scope.playlist.userId = $rootScope.user.id;
         
-        /* Save it in Jukebox */
-        Playlist.create($scope.playlist, function() {
-          $location.path("/playlist/" + $scope.playlist.userId + "/" + $scope.playlist.playlistId);
-        });
-        
-        /* NOW REDIRECT TO IT DAWG */
+      /* Save it in Jukebox */
+      Playlist.create($scope.playlist, function() {
+        /* GET THE PLAYLIST AGAIN */
+        $location.path("/playlist/");
       });
+        
     };
     
   });
--- static/js/app.js
+++ static/js/app.js
@@ -2,38 +2,26 @@ angular
   .module('jukeboxApp', [
     'ngRoute',
     'ngResource',
-    'spotify'
+    'spotify',
+    'underscore'
   ])
   
   .config(function($routeProvider, $locationProvider, SpotifyProvider) {
-    SpotifyProvider.setClientId('829dc5f736a942b4b895ad6cdb6b172e');
+/*    SpotifyProvider.setClientId('829dc5f736a942b4b895ad6cdb6b172e');
     SpotifyProvider.setRedirectUri('http://127.0.0.1:5000/callback');
-    SpotifyProvider.setScope('user-read-private playlist-read-private playlist-modify-private playlist-modify-public');
+    SpotifyProvider.setScope('user-read-private playlist-read-private playlist-modify-private playlist-modify-public'); */
     
     $routeProvider
       .when('/search', {
         templateUrl : 'search.html'
       })
-      .when('/addPlaylist', {
+      .when('/playlists', {
         templateUrl : 'addPlaylist.html'
       })
-      .when('/login', {
-        templateUrl : 'login.html'
-      })
-      .when('/playlist/:userId/:playlistId', {
+      .when('/playlist/:playlistId', {
         templateUrl : 'playlist.html'
       })
       .otherwise({
-        redirectTo: '/login'
+        redirectTo: '/playlists'
       });
   });
-  
-  /* .config(function($routeProvider, $locationProvider) {
-    $routeProvider
-     .when('/search', {
-      templateUrl: 'templates/searchSong.html',
-    })
-    .when('/Book/:bookId/ch/:chapterId', {
-      templateUrl: 'chapter.html',
-      controller: 'ChapterController'
-    }); */
