46704eec8b0393e32713a371e2db2202afe4211a
wiwofone <apeirion@gmail.com>
2014-11-22 20:59:03 +0100

    Get and store playlist on load

--- static/js/playlist.js
+++ static/js/playlist.js
@@ -1,4 +1,9 @@
 angular.module('jukeboxApp')
-  .controller('PlaylistCtrl', function ($rootScope, $scope, $routeParams) {
-    $scope.playlistId = $routeParams;
+  .controller('PlaylistCtrl', function ($rootScope, $scope, $routeParams, Spotify) {
+    $scope.playlistId = $routeParams.playlistId;
+    $scope.userId = $routeParams.userId;
+    
+    Spotify.getPlaylist($scope.userId, $scope.playlistId).then(function (data) {
+      console.log(data);
+    });
   });
