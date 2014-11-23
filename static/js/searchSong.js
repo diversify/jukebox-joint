3ef0e68d9ea3e00d09f624934331d4f51f048f46
wiwofone <apeirion@gmail.com>
2014-11-23 11:23:23 +0100

    Clear search on add and show new list

--- static/js/searchSong.js
+++ static/js/searchSong.js
@@ -1,8 +1,10 @@
 angular.module('jukeboxApp')
-  .controller('SearchSongCtrl', function ($scope, Spotify, $routeParams, $http) {
+  .controller('SearchSongCtrl', function ($scope, Spotify, $routeParams, $http, $route) {
+    $scope.hasAdded = true;
     
     $scope.searchSong = function()
     {
+      $scope.hasAdded = false;
       $scope.searching = true;
       Spotify.search($scope.searchQuery, 'track', {limit: 50}).then(function(data) {
         $scope.searching = false;
@@ -14,7 +16,8 @@ angular.module('jukeboxApp')
     {
       $http.post('/add-song/playlist/' + $routeParams.playlistId + '/track/' + trackId).
         success(function(data, status, headers, config) {
-          
+          $scope.hasAdded = true;
+          $route.reload();
           console.log('Track added');
         }).
         error(function(data, status, headers, config) {
