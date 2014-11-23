455025e3301f9b38e18bd87e500904345d0db773
wiwofone <apeirion@gmail.com>
2014-11-23 10:20:35 +0100

    Correct id references

--- static/js/playlist.js
+++ static/js/playlist.js
@@ -2,6 +2,8 @@ angular.module('jukeboxApp')
   .controller('PlaylistCtrl', function ($rootScope, $scope, $routeParams, Spotify, $http, _, $location) {
     $scope.playlistId = $routeParams.playlistId;
     $scope.userId = 'gaeamearth1';
+    $scope.upvoted = [];
+    $scope.downvoted = [];
     
     $scope.name = $scope.playlistId;
     
@@ -10,11 +12,11 @@ angular.module('jukeboxApp')
           var tracks = data.playlist.tracks;
           $scope.newTracks = [];
           _(tracks).forEach(function(trk) {
-            Spotify.getTrack(trk.ID).then(function (data) {
+            Spotify.getTrack(trk.track_id).then(function (data) {
               var track = data;
               track.voteCount = trk.voteCount;
+              track.dbid = trk.id;
               $scope.newTracks.push(track);
-              console.log(track);
             });
           });
           $scope.tracks = $scope.newTracks;
@@ -26,7 +28,13 @@ angular.module('jukeboxApp')
     
     $scope.upvote = function(track)
     {
-      $http.post('/upvote/playlist/' + $scope.playlistId + '/track/' + track.id).
+      if(typeof $scope.upvoted[track.dbid] !== 'undefined') {
+        return;
+      }
+      
+      $scope.upvoted[track.dbid] = true;
+      
+      $http.post('/upvote/playlist/' + $scope.playlistId + '/track/' + track.dbid).
         success(function(data, status, headers, config) {
           var thisTrack = _.find($scope.tracks, function(trk) {
             return trk.id == track.id;
@@ -43,12 +51,18 @@ angular.module('jukeboxApp')
     
     $scope.downvote = function(track)
     {
+      if(typeof $scope.downvoted[track.dbid] !== 'undefined') {
+        return;
+      }
+      
+      $scope.downvoted[track.dbid] = true;
+      
       var thisTrack = _.find($scope.tracks, function(trk) {
         return trk.id == track.id;
       });
       
       if(thisTrack.voteCount !== 0) {
-          $http.post('/downvote/playlist/' + $scope.playlistId + '/track/' + track.id).
+          $http.post('/downvote/playlist/' + $scope.playlistId + '/track/' + track.dbid).
             success(function(data, status, headers, config) {
               thisTrack.voteCount--;
               console.log('Downvoted');
